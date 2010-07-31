57dd2d94bbe45088118ef43984d95417b8a29dc1
vitogod <vitogod@56b35318-1ecc-11df-854e-63a61b36972e>
2010-07-31 17:15:42 +0000

    Added excel handling proposals

---
+++ trunk/CodeEx/trunk/src/org/vito/test/MapTest.java
@@ -0,0 +1,18 @@
+package org.vito.test;
+
+import java.util.*;
+
+public class MapTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Map map = new HashMap();
+		map.put("1","2");
+		map.put("dog", "Spot");
+		
+		System.out.println(map);
+	}
+
+}
